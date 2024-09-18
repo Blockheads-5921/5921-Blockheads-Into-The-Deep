45707881aae492c85b7fc36eca946279abf2856f
DestinCleveland <dcleveland@lacanadaengineeringclub.org>
2024-09-17 17:04:09 -0700

    joystick destin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseDriveDestin.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Base Drive", group="Drive")
+//@Disabled
+public class BaseDriveDestin extends OpMode {
+    /* Declare OpMode members. */
+    HardwareDrive robot = new HardwareDrive();
+    double joystick = 0;
+    /** The relativeLayout field is used to aid in providing interesting visual feedback
+     * in this sample application; you probably *don't* need this when you use a color sensor on your
+     * robot. Note that you won't see anything change on the Driver Station, only on the Robot Controller. */
+    View relativeLayout;
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+
+        telemetry.addData("Say", "Hello Driver");
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+
+
+    }
+
+    @Override
+    public void loop() {
+        joystick = gamepad1.left_stick_y;
+        robot.rf.setPower(joystick);
+        telemetry.addData("left front encoder position (clicks)", robot.lf.getCurrentPosition());
+        telemetry.addData("left front POWER", robot.lf.getPower());
+        telemetry.addData("joystick Y", joystick);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+}
